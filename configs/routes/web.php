8735a96b53db5f7f2768445e65ea3df5d769032c
Lluis Casamajor <lluis.casamajor@nacex.com>
2023-02-08 10:58:21 +0100

    Creating template and rutes and controller for register and login

--- configs/routes/web.php
+++ configs/routes/web.php
@@ -3,8 +3,15 @@
 declare(strict_types = 1);
 
 use App\Controllers\HomeController;
+use App\Controllers\AuthController;
 use Slim\App;
 
 return function (App $app) {
     $app->get('/', [HomeController::class, 'index']);
+
+    // Autentication
+    $app->get('/login', [AuthController::class, 'loginView']);
+    $app->get('/register', [AuthController::class, 'registerView']);
+    $app->post('/login', [AuthController::class, 'logIn']);
+    $app->post('/register', [AuthController::class, 'register']);
 };
